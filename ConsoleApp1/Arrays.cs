fef4997e919537a2cceb493d8e3e0aa39f604f7d
shubham-k01 <101422378+shubham-k01@users.noreply.github.com>
2024-07-11 23:13:03 +0530

    basic arrays

---
+++ ConsoleApp1/Arrays.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1;
+
+public class Arrays
+{
+    static int[] a = [7,8,15,96,102,54];
+
+    static void Main(){
+        System.Console.WriteLine(a.Length);
+        System.Console.WriteLine(a.First());
+        System.Console.WriteLine(a.ElementAt(1));
+        System.Console.WriteLine(a.All( (x) => x>7 ));
+    }
+}
