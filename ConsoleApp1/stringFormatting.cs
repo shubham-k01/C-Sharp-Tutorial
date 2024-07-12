aad57c9214b153b8d18db296902a79e0263e5583
shubham-k01 <101422378+shubham-k01@users.noreply.github.com>
2024-07-12 22:33:19 +0530

    string formatting

---
+++ ConsoleApp1/stringFormatting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1;
+
+public class stringFormatting
+{
+    static string a = "Hello there";
+    static string? name;
+
+    public void Main(){
+        Console.WriteLine("Enter your name: ");
+        name = Console.ReadLine();
+
+        System.Console.WriteLine(a + name);
+        System.Console.WriteLine("Hello there " + name);
+        System.Console.WriteLine("{0} {1}",a,name);
+        System.Console.WriteLine($"{a} {name}");
+
+    }
+}
