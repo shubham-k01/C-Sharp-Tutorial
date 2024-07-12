cf9a3732e17a441315cf0f097c5fffb90e70fd62
shubham-k01 <101422378+shubham-k01@users.noreply.github.com>
2024-07-12 22:26:41 +0530

    exception handling

---
+++ ConsoleApp1/exceptionHandling.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1;
+
+public class exceptionHandling
+{
+    static int a;
+
+    public void Main(){
+        System.Console.WriteLine("enter a number: ");
+
+        // This line can throw error if user does not enter valid number
+        a = Convert.ToInt32(System.Console.ReadLine());
+
+        try
+        {
+            a = Convert.ToInt32(System.Console.ReadLine());
+        }
+        catch ( System.Exception e)
+        {
+            System.Console.WriteLine(e.Message);
+        }
+
+    }
+}
